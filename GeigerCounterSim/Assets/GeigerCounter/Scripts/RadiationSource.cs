53ea68eccc54ce656d25302bc1acf735751d8d82
Quinn <quinn.daggett@ontariotechu.net>
2020-03-04 21:05:54 -0500

    Adds radiation source and distance to Geiger counter

---
+++ GeigerCounterSim/Assets/GeigerCounter/Scripts/RadiationSource.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+// Script for radiation pocket
+
+public class RadiationSource : MonoBehaviour
+{
+
+	[SerializeField] GameObject counter;
+	[SerializeField] TextMeshPro text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float distance = Vector3.Distance(this.transform.position, counter.transform.position);
+
+		text.text = distance.ToString("F2");
+    }
+}
