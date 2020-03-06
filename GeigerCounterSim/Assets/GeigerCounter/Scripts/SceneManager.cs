8685b3e1574992c15de01c794c64ca8b1bda51eb
Quinn Daggett <quinn.daggett@ontariotechu.net>
2020-03-05 19:33:07 -0500

    Adds player locomotion using Vive controller

---
+++ GeigerCounterSim/Assets/GeigerCounter/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is used to keep track of important things in the scene like time, radiation, and the game's state
+
+public class SceneManager : MonoBehaviour
+{
+	// Object references
+	// Player motor script
+	// List of radiation sources
+	// Timer float
+	// 
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
