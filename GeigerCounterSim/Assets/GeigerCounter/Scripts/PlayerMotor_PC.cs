659305f766860fc983fd6cd2dab974ba7587729c
Quinn Daggett <quinn.daggett@ontariotechu.net>
2020-03-30 22:18:05 -0400

    Porting VR controls to PC

---
+++ GeigerCounterSim/Assets/GeigerCounter/Scripts/PlayerMotor_PC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor_PC : MonoBehaviour
+{
+    // Component references
+    CharacterController controller;
+
+    float moveSpeed;
+    Vector3 movementVector;
+    float xDir;
+    float yDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HandleMovement()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            xDir = 1;
+        }
+    }
+}
